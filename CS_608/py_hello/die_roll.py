98e79237e597a21b53a67d26ab70fcaf800b5dcf
Kautilya <kautilyasave@gmail.com>
2019-09-19 20:05:27 -0400

    CS 608 Python Warmup

---
+++ CS_608/py_hello/die_roll.py
@@ -0,0 +1,17 @@
+import random
+print("Hello Sensehack!")
+
+
+class Die():
+    def roll(self):
+        return random.randint(1, 6)
+
+
+def runDie(times):
+    for x in range(times):
+        # print(x)
+        print(Die().roll())
+
+
+print("Calling Function runDie")
+runDie(6)
---
+++ CS_608/py_hello/hello_world.py
@@ -0,0 +1,27 @@
+ks = "Kautilya"
+msg = "Hello"
+print("Hello Sensehack")
+
+print(msg+" " + ks)
+
+list1 = [1, 4, 6]
+
+print(type(list1))
+print("List1 of 2nd object" + str(list1[2]))
+
+list1.append([8, 9])
+list1.append(9)
+print(type(list1))
+print(list1)
+
+list2 = [5, 24, 24, 5323]
+print("List2 print %s" % list2)
+
+list2.extend([929, 4249])
+print(list2)
+
+strKs = "Kautilya!"
+print(strKs[::-1])
+strKs = strKs[::-1]
+
+print(strKs)
