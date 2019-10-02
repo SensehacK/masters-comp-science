cc9a551bb95de360e3064555b21790a366d222fb
Kautilya <kautilyasave@gmail.com>
2019-10-02 19:58:36 -0400

    Location Access

---
+++ CS_641/Day5/day_5.js
@@ -0,0 +1,22 @@
+console.log("Hello Sensehack!");
+
+function getLocation() {
+    if (!navigator.geolocation) return unsupported();
+    var locText = "Location is ";
+    navigator.geolocation.getCurrentPosition(
+
+        function (pos) {
+            console.log("Position is (" + pos.coords.latitude + ", " + pos.coords.longitude + ")");
+            // locText = pos.coords.latitude + ", " + pos.coords.longitude;
+            // console.log(locText);
+            // locText2 = locText;
+
+            document.getElementById('location').innerHTML = locText + "( " + pos.coords.latitude + ", " + pos.coords.longitude + ")";
+        },
+
+        function error(e) {
+            console.log("Error code " + e.code + " & message " + e.message);
+
+        }
+    )
+}
