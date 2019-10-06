f7d0c21fbaa2b708c6c3bc930053e18b2c7321ee
Kautilya <kautilyasave@gmail.com>
2019-10-06 14:44:54 -0400

    Time Date func

--- CS_641/Assignment_4/assignment_4.js
+++ CS_641/Assignment_4/assignment_4.js
@@ -12,7 +12,33 @@ const repeatPattern = (window.innerWidth) / canvasWidth;
 let colorArr = ['red', 'blue', 'green', 'gray', 'orange', 'black', 'teal', 'purple'];
 var xAxis = 0;
 var yAxis = 0;
+var dateObj = new Date();
+console.log("Hello setting vars");
+console.log(dateObj);
+console.log("The time before init ", dateObj.getMilliseconds());
 
+
+
+
+function init() {
+    console.log("In init func");
+    console.log("Now date ", Date.now());
+    console.log(dateObj);
+    var dateObj2 = new Date();
+
+    console.log("The time it loaded at ", dateObj2.getMilliseconds());
+    console.log("difference in time ", dateObj2 - dateObj);
+
+
+    // For loop for the whole screen dynamically generated
+    for (let i = 0; i < repeatPattern; i++) {
+        // Main Color
+        canvasDraw(colorArr[i % 8], xAxis);
+        // Background Color
+        canvasDraw("aqua", xAxis);
+    }
+
+}
 
 
 
@@ -25,10 +51,4 @@ function canvasDraw(color) {
     xAxis = xAxis + canvasWidth;
 }
 
-// For loop for the whole screen dynamically generated
-for (let i = 0; i < repeatPattern; i++) {
-    // Main Color
-    canvasDraw(colorArr[i % 8], xAxis);
-    // Background Color
-    canvasDraw("aqua", xAxis);
-}
+console.log("Hello for loop");
