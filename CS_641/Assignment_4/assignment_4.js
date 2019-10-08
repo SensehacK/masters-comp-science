4a8b1d3011826c532a0695a23e5ea1dbd28050e9
Kautilya <kautilyasave@gmail.com>
2019-10-08 16:32:53 -0400

    Animation for Canvas : Brain Damaged!

--- CS_641/Assignment_4/assignment_4.js
+++ CS_641/Assignment_4/assignment_4.js
@@ -13,22 +13,18 @@ let colorArr = ['red', 'blue', 'green', 'gray', 'orange', 'black', 'teal', 'purp
 var xAxis = 0;
 var yAxis = 0;
 var dateObj = new Date();
-console.log("Hello setting vars");
 console.log(dateObj);
 console.log("The time before init ", dateObj.getMilliseconds());
 
-
-
-
 function init() {
+
     console.log("In init func");
     console.log("Now date ", Date.now());
     console.log(dateObj);
     var dateObj2 = new Date();
 
     console.log("The time it loaded at ", dateObj2.getMilliseconds());
-    console.log("difference in time ", dateObj2 - dateObj);
-
+    console.log("Difference in time ", dateObj2 - dateObj);
 
     // For loop for the whole screen dynamically generated
     for (let i = 0; i < repeatPattern; i++) {
@@ -38,17 +34,71 @@ function init() {
         canvasDraw("aqua", xAxis);
     }
 
+    xAxis = 0;
+    yAxis = 0;
+    window.addEventListener("devicemotion", handleMotionEvent, true);
+
 }
 
 
-
 // Function for drawing the rectangle
-function canvasDraw(color) {
+function canvasDraw(color, direction) {
+
+    if (xAxis > innerWidth) {
+        xAxis = 0;
+    }
+    // requestAnimationFrame(canvasDraw(color));
+    // myCanvasContext.clearRect(0, 0, innerWidth, innerHeight);
     myCanvasContext.fillStyle = color;
     // Using Global Variables
     myCanvasContext.fillRect(xAxis, yAxis, canvasWidth, window.innerHeight);
-    // Adding X - axis with respect to width between each rectangle
+
     xAxis = xAxis + canvasWidth;
+
+}
+
+// Animation function
+function handleMotionEvent(event) {
+
+    // Resetting the xAxis when reached its limit of Window width
+    if (xAxis > innerWidth) {
+        xAxis = 0;
+    }
+
+    // Getting Accelerometer X & Y axis
+    var x = event.accelerationIncludingGravity.x;
+    var y = event.accelerationIncludingGravity.y;
+
+    if ((x > 1 && x < 3) || (x < -1 && x > -3)) {
+
+        console.log("printing x greater or smaller than 1 ", x);
+
+        xAxis = (x > 1) ? xAxis + (x * 0.05) : xAxis - (x * 0.05);
+        // Y- axis could be utilized but it doesn't translate well with the animation.
+        yAxis = (y > 1) ? yAxis + (y * 0.05) : yAxis - (y * 0.05);
+        // For loop for the whole screen dynamically generated
+        for (let i = 0; i < repeatPattern; i++) {
+            // Main Color
+            canvasDraw(colorArr[i % 8], xAxis);
+            // Background Color
+            canvasDraw("aqua", xAxis);
+        }
+    }
+
+    if (x > 3 || x < -3) {
+
+        console.log("printing x greater or smaller than 3 ", x);
+
+        xAxis = (x > 3) ? xAxis + (x * 0.3) : xAxis - (x * 0.3);
+
+        for (let i = 0; i < repeatPattern; i++) {
+            // Main Color
+            canvasDraw(colorArr[i % 8], xAxis);
+            // Background Color
+            canvasDraw("aqua", xAxis);
+        }
+    }
+
 }
 
 console.log("Hello for loop");
--- CS_641/Day5/day_5.js
+++ CS_641/Day5/day_5.js
@@ -6,13 +6,14 @@ function getLocation() {
     navigator.geolocation.getCurrentPosition(
 
         function (pos) {
+
             console.log("Position is (" + pos.coords.latitude + ", " + pos.coords.longitude + ")");
-
-
             document.getElementById('location').innerHTML = locText + "( " + pos.coords.latitude + ", " + pos.coords.longitude + ")";
+
         },
 
         function error(e) {
+
             console.log("Error code " + e.code + " & message " + e.message);
 
         }
