1edd4362f098a53eb959c3a2559c73a471d7550a
Kautilya <kautilyasave@gmail.com>
2019-10-11 18:47:43 -0400

    Formatting differences & trying iOS 13 gestures permissions.

--- CS_641/Assignment_5/assignment_5.js
+++ CS_641/Assignment_5/assignment_5.js
@@ -1,15 +1,24 @@
 // Defining Variables
-let myCanvas = document.getElementById('myCanvas');
+let myCanvas = document.getElementById("myCanvas");
 let myCanvasContext = myCanvas.getContext("2d");
 myCanvas.width = window.innerWidth;
 myCanvas.height = window.innerHeight;
 
 // Setting up constants
 const canvasWidth = 15;
-const repeatPattern = (window.innerWidth) / canvasWidth;
+const repeatPattern = window.innerWidth / canvasWidth;
 
 // Setting Variables.
-let colorArr = ['red', 'blue', 'green', 'gray', 'orange', 'black', 'teal', 'purple'];
+let colorArr = [
+  "red",
+  "blue",
+  "green",
+  "gray",
+  "orange",
+  "black",
+  "teal",
+  "purple"
+];
 var xAxis = 0;
 var yAxis = 0;
 var dateObj = new Date();
@@ -17,92 +26,110 @@ console.log(dateObj);
 console.log("The time before init ", dateObj.getMilliseconds());
 
 function init() {
+  console.log("In init func");
+  console.log("Now date ", Date.now());
+  console.log(dateObj);
+  var dateObj2 = new Date();
 
-    console.log("In init func");
-    console.log("Now date ", Date.now());
-    console.log(dateObj);
-    var dateObj2 = new Date();
+  console.log("The time it loaded at ", dateObj2.getMilliseconds());
+  console.log("Difference in time ", dateObj2 - dateObj);
 
-    console.log("The time it loaded at ", dateObj2.getMilliseconds());
-    console.log("Difference in time ", dateObj2 - dateObj);
+  // For loop for the whole screen dynamically generated
+  for (let i = 0; i < repeatPattern; i++) {
+    // Main Color
+    canvasDraw(colorArr[i % 8], xAxis);
+    // Background Color
+    canvasDraw("aqua", xAxis);
+  }
 
-    // For loop for the whole screen dynamically generated
-    for (let i = 0; i < repeatPattern; i++) {
-        // Main Color
-        canvasDraw(colorArr[i % 8], xAxis);
-        // Background Color
-        canvasDraw("aqua", xAxis);
-    }
+  xAxis = 0;
+  yAxis = 0;
+  //   window.addEventListener("devicemotion", handleMotionEvent, true);
 
-    xAxis = 0;
-    yAxis = 0;
-    window.addEventListener("devicemotion", handleMotionEvent, true);
-
+  safariIOS();
 }
-
 
 // Function for drawing the rectangle
 function canvasDraw(color, direction) {
+  if (xAxis > innerWidth) {
+    xAxis = 0;
+  }
+  // For some reason it returns a blank page.
+  // requestAnimationFrame(canvasDraw(color));
 
-    if (xAxis > innerWidth) {
-        xAxis = 0;
-    }
-    // For some reason it returns a blank page.
-    // requestAnimationFrame(canvasDraw(color));
+  // myCanvasContext.clearRect(xAxis, yAxis, canvasWidth - 10, innerHeight);
+  myCanvasContext.fillStyle = color;
 
-    // myCanvasContext.clearRect(xAxis, yAxis, canvasWidth - 10, innerHeight);
-    myCanvasContext.fillStyle = color;
+  // Using Global Variables
+  myCanvasContext.fillRect(xAxis, yAxis, canvasWidth, window.innerHeight);
 
-    // Using Global Variables
-    myCanvasContext.fillRect(xAxis, yAxis, canvasWidth, window.innerHeight);
-
-    xAxis = xAxis + canvasWidth;
-
+  xAxis = xAxis + canvasWidth;
 }
 
 // Animation function
 function handleMotionEvent(event) {
+  // Resetting the xAxis when reached its limit of Window width
+  if (xAxis > innerWidth) {
+    xAxis = 0;
+  }
 
-    // Resetting the xAxis when reached its limit of Window width
-    if (xAxis > innerWidth) {
-        xAxis = 0;
+  // Getting Accelerometer X & Y axis
+  // var x = event.accelerationIncludingGravity.x;
+  var y = event.accelerationIncludingGravity.y;
+
+  if ((x > 1 && x < 3) || (x < -1 && x > -3)) {
+    console.log("printing x greater or smaller than 1 ", x);
+
+    xAxis = x > 1 ? xAxis + x * 0.05 : xAxis - x * 0.05;
+    // Y- axis could be utilized but it doesn't translate well with the animation.
+    // yAxis = (y > 1) ? yAxis + (y * 0.05) : yAxis - (y * 0.05);
+
+    // For loop for the whole screen dynamically generated
+    for (let i = 0; i < repeatPattern; i++) {
+      // Main Color
+      canvasDraw(colorArr[i % 8], xAxis);
+      // Background Color
+      canvasDraw("aqua", xAxis);
     }
+  }
 
-    // Getting Accelerometer X & Y axis
-    var x = event.accelerationIncludingGravity.x;
-    var y = event.accelerationIncludingGravity.y;
+  if (x > 3 || x < -3) {
+    console.log("printing x greater or smaller than 3 ", x);
 
-    if ((x > 1 && x < 3) || (x < -1 && x > -3)) {
+    xAxis = x > 3 ? xAxis + x * 0.3 : xAxis - x * 0.3;
 
-        console.log("printing x greater or smaller than 1 ", x);
-
-        xAxis = (x > 1) ? xAxis + (x * 0.05) : xAxis - (x * 0.05);
-        // Y- axis could be utilized but it doesn't translate well with the animation.
-        // yAxis = (y > 1) ? yAxis + (y * 0.05) : yAxis - (y * 0.05);
-
-        // For loop for the whole screen dynamically generated
-        for (let i = 0; i < repeatPattern; i++) {
-            // Main Color
-            canvasDraw(colorArr[i % 8], xAxis);
-            // Background Color
-            canvasDraw("aqua", xAxis);
-        }
+    for (let i = 0; i < repeatPattern; i++) {
+      // Main Color
+      canvasDraw(colorArr[i % 8], xAxis);
+      // Background Color
+      canvasDraw("aqua", xAxis);
     }
+  }
+}
 
-    if (x > 3 || x < -3) {
+function safariIOS(event) {
+  var deviceMotionEvent = new DeviceMotionEvent(
+    type[("devicemotion", options)]
+  );
 
-        console.log("printing x greater or smaller than 3 ", x);
+  DeviceMotionEvent.requestPermission()
+    .then(response => {
+      if (response == "granted") {
+        window.addEventListener("devicemotion", e => {
+          console.log(e);
+          // do something with e
+        });
+      }
+    })
+    .catch(console.error);
 
-        xAxis = (x > 3) ? xAxis + (x * 0.3) : xAxis - (x * 0.3);
-
-        for (let i = 0; i < repeatPattern; i++) {
-            // Main Color
-            canvasDraw(colorArr[i % 8], xAxis);
-            // Background Color
-            canvasDraw("aqua", xAxis);
-        }
+  deviceMotionEvent.requestPermission().then(response => {
+    if (response == "granted") {
+      window.addEventListener("devicemotion", e => {
+        console.log(e);
+      });
     }
-
+  });
 }
 
 console.log("Hello for loop");
--- CS_641/Day5/day_5.js
+++ CS_641/Day5/day_5.js
@@ -1,21 +1,45 @@
 console.log("Hello Sensehack!");
 
 function getLocation() {
-    if (!navigator.geolocation) return unsupported();
-    var locText = "Location is ";
-    navigator.geolocation.getCurrentPosition(
+  if (!navigator.geolocation) return unsupported();
+  var locText = "Location is ";
+  navigator.geolocation.getCurrentPosition(
+    function (pos) {
+      console.log(
+        "Position is (" +
+        pos.coords.latitude +
+        ", " +
+        pos.coords.longitude +
+        ")"
+      );
+      document.getElementById("location").innerHTML =
+        locText +
+        "( " +
+        pos.coords.latitude +
+        ", " +
+        pos.coords.longitude +
+        ")";
+    },
 
-        function (pos) {
+    function error(e) {
+      console.log("Error code " + e.code + " & message " + e.message);
+    }
+  );
+}
 
-            console.log("Position is (" + pos.coords.latitude + ", " + pos.coords.longitude + ")");
-            document.getElementById('location').innerHTML = locText + "( " + pos.coords.latitude + ", " + pos.coords.longitude + ")";
+function safariIOS(params) {
+  var deviceMotionEvent = new DeviceMotionEvent(
+    type[(deviceMotionEvent, options)]
+  );
 
-        },
-
-        function error(e) {
-
-            console.log("Error code " + e.code + " & message " + e.message);
-
-        }
-    )
+  DeviceMotionEvent.requestPermission()
+    .then(response => {
+      if (response == "granted") {
+        window.addEventListener("devicemotion", e => {
+          console.log(e);
+          // do something with e
+        });
+      }
+    })
+    .catch(console.error);
 }
