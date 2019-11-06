39e7bc72f537e29992bc01bbf38143aa6ddd425e
Kautilya <kautilyasave@gmail.com>
2019-11-05 20:43:27 -0500

    (Feat) Ass_7: Adding some components
    -UIAction Controller
    -UIModal Controller
    -ngModel
    -ionIcon
    -ionCard

--- CS_641/Assignment_7/ass7/src/app/home/home.page.ts
+++ CS_641/Assignment_7/ass7/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ActionSheetController } from '@ionic/angular';
 import { ModalPage } from '../modal/modal.page';
 
 @Component({
@@ -9,7 +9,10 @@ import { ModalPage } from '../modal/modal.page';
 })
 export class HomePage {
 
-  constructor(private modal: ModalController) { }
+  name: string;
+
+  constructor(private modal: ModalController,
+    private actionSheetController: ActionSheetController) { }
 
 
   onClick() {
@@ -20,12 +23,54 @@ export class HomePage {
   async onModal() {
 
     const modal = await this.modal.create({
-      component: ModalPage
+      component: ModalPage,
+      componentProps: {
+        data: this.name,
+      }
     });
 
-    return await modal.present();
+    await modal.present();
 
+    modal.onDidDismiss()
+      .then(resp => {
+        alert(JSON.stringify(resp.data));
+      });
 
 
   }
+
+  async actionSheet() {
+    console.log('Hello Actionsheet');
+
+    const actionSheet = this.actionSheetController.create({
+      header: 'Websites',
+      buttons: [{
+        text: 'Reddit',
+        role: 'open',
+        handler: () => {
+          console.log('Open clicked');
+
+        }
+      }, {
+        text: 'Spotify',
+        role: 'music',
+        handler: () => {
+          console.log('Play Stairway to heaven');
+
+        }
+      }, {
+        text: 'Dash',
+        role: 'dash',
+        handler: () => {
+          console.log('Download Dash API');
+        }
+      }]
+    });
+
+    (await actionSheet).present();
+  }
+
+
+
+
 }
--- CS_641/Assignment_7/ass7/src/app/modal/modal.page.ts
+++ CS_641/Assignment_7/ass7/src/app/modal/modal.page.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { NavParams } from '@ionic/angular';
+import { NavParams, ModalController } from '@ionic/angular';
 
 @Component({
     selector: 'modal-page',
@@ -7,13 +7,34 @@ import { NavParams } from '@ionic/angular';
 })
 
 export class ModalPage implements OnInit {
+    @Input() data: any;
 
-    constructor() {
+    constructor(private modalCtrl: ModalController) {
         console.log('Yo!');
 
     }
 
-    ngOnInit() { }
+    ngOnInit() {
+        this.data = this.data.toLowerCase;
+        if (!this.data) {
+            this.data = 'Kautilya';
+        }
+        else if (this.data === 'haik' || this.data === 'prakash') {
+            this.data = this.data + '😂😬';
+            console.log('Hope I\'m doing alright!');
+            console.log('P.S: You should checkout  https://electronjs.org/ ');
 
+
+        }
+
+    }
+
+
+    dismissModal() {
+        this.modalCtrl.dismiss({
+            name: 'Sensehack',
+            name2: 'Kautilya',
+        });
+    }
 }
 
