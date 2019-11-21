c23ebcb7b2a3bb76724b25d6a022cac68619f9bc
Kautilya <kautilyasave@gmail.com>
2019-11-21 12:09:52 -0500

    CS_641: Ionic Native & Xcode Works

--- CS_641/Day_11/ionic_native/src/app/app.module.ts
+++ CS_641/Day_11/ionic_native/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Vibration } from '@ionic-native/vibration/ngx';
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,8 +18,10 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    Vibration,
+    TextToSpeech,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- CS_641/Day_11/ionic_native/src/app/home/home.page.ts
+++ CS_641/Day_11/ionic_native/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Vibration } from '@ionic-native/vibration/ngx';
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private vibration: Vibration,
+    private speech: TextToSpeech) { }
+
+
+  vibratePhone() {
+    console.log('Hello Sensehack!');
+    alert('Hello');
+    this.vibration.vibrate(1000);
+  }
+
+  speakPhone() {
+    console.log('In speak phone');
+    this.speech.speak('Dev App')
+      .then(() => {
+        console.log('Worked fine');
+      })
+      .catch(() => {
+        console.log('Catch error');
+      });
+  }
+
+
 
 }
