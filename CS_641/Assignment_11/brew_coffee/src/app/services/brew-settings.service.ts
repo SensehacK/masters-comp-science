8c9390820b76dd7daa91595b58010dcf019d7f16
Kautilya <kautilyasave@gmail.com>
2019-12-18 14:29:30 -0500

    Revert "Added Interface"
    This reverts commit 228294c5029f9fb27e9cac44e7f5519ee6513736.

--- CS_641/Assignment_11/brew_coffee/src/app/services/brew-settings.service.ts
+++ CS_641/Assignment_11/brew_coffee/src/app/services/brew-settings.service.ts
@@ -2,8 +2,6 @@ import { Params } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 
-import { CoffeeSync } from '../model/brew_coffee'
-
 
 @Injectable({
   providedIn: 'root'
@@ -14,16 +12,6 @@ export class BrewSettingsService {
   private firstLaunch = true;
   private trackAppLaunches: number;
   private coffeeData: {};
-
-  private appSettingsSync: {
-    gitCodeURL: '',
-    appTodayMsg: '',
-    greetMsg: '',
-  };
-
-  private coffeeAppInterface: CoffeeSync;
-
-
 
 
   constructor(private storage: Storage) {
